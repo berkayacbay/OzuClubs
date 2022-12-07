25d09064b3657224cfab6c97cf39cfb5823848f3
gunesaltiner <gunes.altiner@ozu.edu.tr>
2022-12-07 13:14:56 +0300

    About Fragment

--- app/src/main/java/com/example/ozuclubs/MainActivity.kt
+++ app/src/main/java/com/example/ozuclubs/MainActivity.kt
@@ -9,15 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-   //     replaceFragment(ClubsFragment())
     }
-/*
-    private fun replaceFragment(recyclerViewFragment: Fragment) {
-        val fragmentManager =supportFragmentManager
-        val fragmentTransaction=fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,recyclerViewFragment)
-        fragmentTransaction.commit()
 
-    }
-    */
+
 }
