653d59577c290f19d892154eaf3f7c32e9b6c09f
berkayacbay <berkay.acbay@ozu.edu.tr>
2022-12-07 12:38:56 +0300

    EventsFragment

--- app/src/main/java/com/example/ozuclubs/fragments/EventsFragment.kt
+++ app/src/main/java/com/example/ozuclubs/fragments/EventsFragment.kt
@@ -6,16 +6,64 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ozuclubs.R
+import com.example.ozuclubs.adapter.ClubAdapter
+import com.example.ozuclubs.adapter.EventAdapter
+import com.example.ozuclubs.data.Club
+import com.example.ozuclubs.data.Event
 import com.example.ozuclubs.databinding.FragmentEventsBinding
 
 class EventsFragment: Fragment() {
+    private  lateinit var adapter: EventAdapter
+    private  lateinit var rv: RecyclerView
+    private  lateinit var eventsArrayList:ArrayList<Event>
+
+    lateinit var imageId: Array<Int>
+    lateinit var eventName: Array<String>
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentEventsBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_events, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataInitialize()
+        val layoutManager = LinearLayoutManager(context)
+         rv=view.findViewById(R.id.recycler_view2)
+        rv.layoutManager=layoutManager
+        rv.setHasFixedSize(true)
+        adapter= EventAdapter(eventsArrayList)
+        rv.adapter=adapter
+    }
+
+    private fun dataInitialize(){
+        var eventsArrayList= arrayListOf<Event>()
+        var imageId= arrayOf(
+            R.drawable.image_a,
+            R.drawable.image_b,
+            R.drawable.image_c,
+            R.drawable.image_d,
+            R.drawable.image_e,
+            R.drawable.image_f,
+        )
+        var eventName= arrayOf(
+            getString(R.string.description_a),
+            getString(R.string.description_b),
+            getString(R.string.description_c),
+            getString(R.string.description_d),
+            getString(R.string.description_e),
+            getString(R.string.description_f)
+        )
+        for(i in imageId.indices){
+            val event= Event(eventName[i],imageId[i])
+            eventsArrayList.add(event)
+        }
+
     }
 
 }
