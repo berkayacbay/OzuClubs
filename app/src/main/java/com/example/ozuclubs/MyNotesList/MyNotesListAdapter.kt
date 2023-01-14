2cbaf29398978a10e4263d350648a9c1545c500c
tugceozgirgin <93055813+tugceozgirgin@users.noreply.github.com>
2023-01-14 15:39:55 +0300

    mylist cardview added

--- app/src/main/java/com/example/ozuclubs/MyNotesList/MyNotesListAdapter.kt
+++ app/src/main/java/com/example/ozuclubs/MyNotesList/MyNotesListAdapter.kt
@@ -12,26 +12,15 @@ import com.example.ozuclubs.R
 import com.example.ozuclubs.databinding.ListMylistBinding
 
 class MyListAdapter: ListAdapter<Note,MyListAdapter.MyViewHolder>(MyListDiffCallback()) {
-//     var data =  listOf<Note>()
-//          set(value) {
-//               field = value
-//               notifyDataSetChanged()
-//          }
 
      class MyViewHolder(val binding: ListMylistBinding): RecyclerView.ViewHolder(binding.root){
 
 
           val noteId: TextView = binding.textNoteId
-          val noteTitle: TextView = binding.textTitle
-          val noteDesc: TextView = binding.textDesc
-          val noteTime: TextView = binding.textTime
-          val imageNote: ImageView = binding.imageNote
 
           companion object {
                fun from(parent: ViewGroup): MyViewHolder {
                    val layoutInflater = LayoutInflater.from(parent.context)
-//                    val view = layoutInflater
-//                         .inflate(R.layout.list_mylist, parent, false)
                     val binding = ListMylistBinding.inflate(layoutInflater,parent,false)
                     return MyViewHolder(binding)
                }
@@ -40,29 +29,11 @@ class MyListAdapter: ListAdapter<Note,MyListAdapter.MyViewHolder>(MyListDiffCall
           fun bind(note: Note){
                noteId.text = note.id.toString()
                binding.note = note
-//               noteTitle.text =note.note_title
-//               noteDesc.text = note.note_description
-//               noteTime.text = note.note_time
-
-
-
-//               imageNote.setImageResource(when (note.id %6 ){
-//                    0 -> R.drawable.note1
-//                    1 -> R.drawable.note2
-//                    2 -> R.drawable.note3
-//                    3 -> R.drawable.note4
-//                    4 -> R.drawable.note5
-//                    5 -> R.drawable.note6
-//                    else-> R.drawable.note1
-//               })
-
 
           }
      }
 
      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-//          val layout = LayoutInflater.from(parent.context).inflate(R.layout.list_mylist, parent,false)
-//          return MyListAdapter.MyViewHolder(layout)
           return MyViewHolder.from(parent)
      }
 
@@ -70,15 +41,8 @@ class MyListAdapter: ListAdapter<Note,MyListAdapter.MyViewHolder>(MyListDiffCall
      override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
           val currentItem = getItem(position)
           holder.bind(currentItem)
-//          holder.noteId.text = currentItem.id.toString()
-//          holder.noteTitle.text = currentItem.note_title
-//          holder.noteDesc.text = currentItem.note_description
-//          holder.noteTime.text = currentItem.note_time
 
      }
-
-
-
 
 }
 class MyListDiffCallback : DiffUtil.ItemCallback<Note>(){
